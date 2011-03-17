d05ff5c0ec459dc840dd97f77d79667a82b1ded5
mavery <mavery@e170004d-70c3-49c3-837d-8c0469307a96>
2011-03-17 11:29:14 +0000

    Handle newlines when building strings.

--- src/com/advancedpwr/record/methods/StringBuilder.java
+++ src/com/advancedpwr/record/methods/StringBuilder.java
@@ -20,7 +20,7 @@ public class StringBuilder extends AbstractPrimitiveBuilder
 	
 	public String resultBuilder()
 	{
-		return "\"" + result().replaceAll( "\n", "\\\\n" ) + "\""; 
+		return "\"" + result().replaceAll( "\n", "\\\\n" ).replaceAll( "\r", "\\\\r" ) + "\""; 
 	}
 
 
