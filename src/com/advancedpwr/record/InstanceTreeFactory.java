5d606bc7d253e079e5e2a4bcab73c28d05e5a339
Hani Suleiman <hani@formicary.net>
2012-01-28 21:48:46 -0500

    Use IdentityHashMap, we don't want a screwy .equals/.hashCode to cause drama

--- src/com/advancedpwr/record/InstanceTreeFactory.java
+++ src/com/advancedpwr/record/InstanceTreeFactory.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 Matthew Avery, mavery@advancedpwr.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,26 +15,22 @@
  */
 package com.advancedpwr.record;
 
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class InstanceTreeFactory
 {
 	protected Map<Object, InstanceTree> fieldTrees;
 	protected int count;
-	
+
 	protected Map<Object, InstanceTree> getTrees()
 	{
 		if ( fieldTrees == null )
 		{
-			fieldTrees = new LinkedHashMap<Object, InstanceTree>();
+			fieldTrees = new IdentityHashMap<Object, InstanceTree>();
 		}
 		return fieldTrees;
 	}
-	
+
 	public InstanceTree createInstanceTree( Object result, InstanceTree inParent )
 	{
 		InstanceTree tree = getTrees().get( result );
