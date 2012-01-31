1a0f3f95774cdcfe5492041fce9004cc92684842
Hani Suleiman <hani@formicary.net>
2012-01-30 20:52:06 -0500

    Half-hearted attempt at fixing mock failing tests

--- src/main/java/com/advancedpwr/record/AccessPath.java
+++ src/main/java/com/advancedpwr/record/AccessPath.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 Matthew Avery, mavery@advancedpwr.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,10 +20,10 @@ import java.util.Set;
 
 /**
  * <code>AccessPath</code> represents the code path leading to the access of an object in the {@link InstanceTree}
- * 
+ *
  * Instances of <code>AccessPath</code> are inspected by an {@link ObjectRecorder} at class generation time to determine
  * how to reconstruct the object tree.
- * 
+ *
  * @author Matthew Avery, mavery@advancedpwr.com on May 29, 2010
  */
 public class AccessPath
@@ -34,7 +34,7 @@ public class AccessPath
 	{
 		return getInstanceTree().getObject();
 	}
-	
+
 	public void setTree( InstanceTree inTree )
 	{
 		fieldInstanceTree = inTree;
@@ -45,29 +45,33 @@ public class AccessPath
 	{
 		return getInstanceTree().objectClass();
 	}
-	
+
 	// Return type of the access path, e.g. List
 	public Class getReturnType()
 	{
 		return getResultClass();
 	}
-	
+
 	// Parameter type
 	public Class getParameterClass()
 	{
 		return getResultClass();
 	}
-	
+
 	public InstanceTree getInstanceTree()
 	{
 		return fieldInstanceTree;
 	}
-	
+
 	public String nameRoot()
 	{
-		return getResultClass().getSimpleName() + suffix();
+    Class resultClass = getResultClass();
+    if(resultClass.getSimpleName().contains("$$EnhancerByCGLIB$")) {
+      resultClass = resultClass.getSuperclass();
+    }
+    return resultClass.getSimpleName() + suffix();
 	}
-	
+
 	public String suffix()
 	{
 		int index = getInstanceTree().getIndex();
@@ -82,7 +86,7 @@ public class AccessPath
 	{
 		return "";
 	}
-	
+
 	public String toString()
 	{
 		return getClass().getSimpleName() + ": " + getInstanceTree().getObject() + " instance " + getInstanceTree();
@@ -92,5 +96,5 @@ public class AccessPath
 	{
 		return new LinkedHashSet<Class>();
 	}
-		
+
 }
